9be339cb23602a752a596f2eb0b47b8a641dd11f
Jocelyn FOUCHER <jfoucher@e1r10p16.42.fr>
2020-08-17 22:51:27 +0200

    installed phpmyadmin and wordpress but not working yet

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,14 +1,10 @@
 service mysql start
-mysql -u root -e "CREATE DATABASE wordpress;"
-mysql wordpress -e "CREATE USER 'jfoucher'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root  -e "GRANT ALL ON wordpress.* TO 'jfoucher'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+mysql < /usr/share/phpmyadmin/sql/create_tables.sql -u root -p
+mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'jfoucher'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "CREATE TABLE wordpress.todo_list (
-    item_id INT AUTO_INCREMENT,
-    content VARCHAR(255),
-    PRIMARY KEY(item_id)
-);"
-mysql -u root -e "INSERT INTO wordpress.todo_list (content) VALUES (\"My first important item\");"
-mysql -u root -e "SELECT * FROM wordpress.todo_list;"
 service php7.3-fpm start
+mysql -u root -e "CREATE DATABASE app_db;
+GRANT ALL PRIVILEGES ON app_db.* TO 'app_user'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;"
 nginx -g 'daemon off;'
